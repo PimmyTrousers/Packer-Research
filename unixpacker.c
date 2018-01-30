69ae68d0cb13fe2544cde51029b083c46ecc31e3
PimmyTrousers <pim.trouerbach1@gmail.com>
2018-01-30 11:32:03 -0800

    Unix packer works perfectly

--- unixpacker.c
+++ unixpacker.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/stat.h>
+
+#define MODE 0777
 
 char* stub_crypt(FILE* hFile, int dwFileSize);
 
 char CRYPTED_FILE[15] = "crypto.bin";
+char UNCRYPTED_FILE[15] = "uncrpyto.bin";
 char Key[9];
 
 int main(int argc, char* argv[])
@@ -24,11 +28,18 @@ int main(int argc, char* argv[])
     {
         // get crypted file name in current directory
         char cwd[1024];
+        char un_cwd[1024];
         getcwd(cwd, sizeof(cwd));
         strcat(cwd, "/");
         strcat(cwd, CRYPTED_FILE);
-        printf("%s\n", cwd);
-        // open handle to new decrypted file
+        printf("new file, %s\n", cwd);
+
+        // create location for new unencrypted file
+        getcwd(un_cwd, sizeof(cwd));
+        strcat(un_cwd, "/");
+        strcat(un_cwd, UNCRYPTED_FILE);
+       
+        // open handle to crypted file
         FILE *hCryptedFile = fopen(cwd, "ab+");
         if (hCryptedFile == NULL)
         {
@@ -36,31 +47,35 @@ int main(int argc, char* argv[])
             exit(1);
         }
 
-        // get file size, remove the file and close the handle
+        // get file size of the crypted file
         fseek(hCryptedFile, 0, SEEK_END);
         int size = ftell(hCryptedFile);
-        printf("The size is 0x%x\n", size);
         fseek(hCryptedFile, 0, SEEK_SET);
 
+        // create the buffer with decrypted contents and close the handle to the crypted file
         char *lpFileBytes = stub_crypt(hCryptedFile, size);
-
         fclose(hCryptedFile);
-        hCryptedFile = fopen(cwd, "wb");
-        // Create the file
+
+        // create a handle to the unpacked executable 
+        hCryptedFile = fopen(un_cwd, "wb");
         if (hCryptedFile == NULL)
         {
             fputs("Opening the file failed", stderr);
             exit(1);
         }
 
-        // write result to file
+        // write result to unencrypted file
         int result = fwrite(lpFileBytes, size, 1, hCryptedFile);
         fclose(hCryptedFile);
         free(lpFileBytes);
 
+        // change permissions of unpacked file, run it and finalyl remove it
+        chmod(un_cwd, MODE);
+        system(un_cwd);
+        remove(un_cwd);
  }else{
-   // crypter routine
-       // open file to crypt
+       // packer routine
+       // open file to pack
        FILE* hFile = fopen(argv[1], "ab+");
        if(hFile == NULL){
            fputs("Opening the file failed", stderr);
@@ -71,24 +86,24 @@ int main(int argc, char* argv[])
        int size = ftell(hFile);
        fseek(hFile, 0, SEEK_SET);
 
-       
+       // pack and get packed bytes
+       char* lpFileBytes = stub_crypt(hFile, size);
 
-       // crypt and get crypted bytes
-      char* lpFileBytes = stub_crypt(hFile, size);
-
-       // get crypted file name in current directory
+       // get packed file name in current directory
        char cwd[1024];
        getcwd(cwd, sizeof(cwd));
        strcat(cwd, "/");
        strcat(cwd, CRYPTED_FILE);
        printf("%s", cwd);
-       // open handle to new crypted file
+
+       // open handle to new packed file
        FILE* hCryptedFile = fopen(cwd, "wb");
-      //  write to crypted file
-      int result = fwrite(lpFileBytes, size, 1, hCryptedFile);
-      fclose(hCryptedFile);
-      free(lpFileBytes);
- }
+
+       //  write to packed file
+       int result = fwrite(lpFileBytes, size, 1, hCryptedFile);
+       fclose(hCryptedFile);
+       free(lpFileBytes);
+     }
 }
 
 char* stub_crypt(FILE* hFile, int dwFileSize)
@@ -98,7 +113,6 @@ char* stub_crypt(FILE* hFile, int dwFileSize)
     buffer = (char*)calloc(dwFileSize,sizeof(char));
 
     // read the file into the buffer
-    printf("The size is 0x%x\n", dwFileSize);
     if( 1!=fread( buffer , dwFileSize, 1 , hFile)){
       fclose(hFile);
       free(buffer);
